f0ddb683d37f33920bd75204c8f639465139b00a
JaeChen <470494530@qq.com>
2018-11-16 22:36:28 +0800

    ColorSpace/colorChange.py

--- ColorSpace/colorChange.py
+++ ColorSpace/colorChange.py
@@ -3,10 +3,16 @@
 
 import cv2
 
-def colorChage():
-    src_img = cv2.imread()
-    pass
+def colorChange():
+    src_img = cv2.imread('D:/win10/cv/cv/Data\Images/000.jpg', 1)
+    if src_img is not None:
+        gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('src_image', src_img)
+        cv2.waitKey(0)
+        cv2.imshow('gray_image', gray_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    colorChage()
+    colorChange()
